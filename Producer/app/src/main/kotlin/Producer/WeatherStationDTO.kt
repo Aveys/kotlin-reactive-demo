bb2b2b6b9bb2cb973b76eceab12cc54d3b86a219
Arthur Veys <veysarthur@gmail.com>
2022-04-26 10:47:21 -0400

    Modify deserializer

--- Producer/app/src/main/kotlin/Producer/WeatherStationDTO.kt
+++ Producer/app/src/main/kotlin/Producer/WeatherStationDTO.kt
@@ -1,13 +1,23 @@
 package Producer
 
+import com.fasterxml.jackson.databind.MappingIterator
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 
 data class WeatherStationDTO(val id: String, val name: String, val location: String)
 
 object WeatherStationDTODeserializer : ResponseDeserializable<List<WeatherStationDTO>> {
-    override fun deserialize(content: String) =
-        ObjectMapper().registerKotlinModule().readValue<List<WeatherStationDTO>>(content)
+    override fun deserialize(content: String): List<WeatherStationDTO> {
+        val results = mutableListOf<WeatherStationDTO>()
+        val mapper = ObjectMapper().registerKotlinModule()
+        val values: MappingIterator<WeatherStationDTO> = mapper.readerFor(WeatherStationDTO::class.java)
+            .readValues(content)
+        values.use {
+            while (it.hasNextValue()) {
+                results.add(it.nextValue())
+            }
+        }
+        return results
+    }
 }
